ca6d90672d1e5308de831ba769aa708cdf31000e
sunjw <jwsun@sunjwdeMacBook-Pro.local>
2018-11-16 22:24:20 +0800

    sql调用由jpautil更改为jdbcExecutors

--- common-boost/src/main/java/top/wboost/common/boost/handler/AbstractSqlBoostHandler.java
+++ common-boost/src/main/java/top/wboost/common/boost/handler/AbstractSqlBoostHandler.java
@@ -1,18 +1,22 @@
 package top.wboost.common.boost.handler;
 
-import java.util.List;
-import java.util.Map;
-
 import top.wboost.common.sql.manager.DefaultSqlWildCardManager;
 import top.wboost.common.sql.warp.DefaultSqlWarp;
-import top.wboost.common.util.JpaUtil;
+import top.wboost.common.util.JdbcBuilder;
 import top.wboost.common.utils.web.utils.ConvertUtil;
+import top.wboost.common.utils.web.utils.SpringBeanUtil;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("unchecked")
 public abstract class AbstractSqlBoostHandler extends AbstractBoostHandler {
 
     private DefaultSqlWildCardManager sqlWildCardManager = new DefaultSqlWildCardManager(new DefaultSqlWarp());
 
+    private JdbcBuilder.JdbcExecutor jdbcExecutor = JdbcBuilder.createExecutors(SpringBeanUtil.getBean(DataSource.class));
     /**
      * 执行sql语句
      * @param sql 要执行的sql语句
@@ -21,7 +25,11 @@ public abstract class AbstractSqlBoostHandler extends AbstractBoostHandler {
     protected List<?> executeSql(Map<String, Object> replaceMap, String sql) {
         String executeSql = replaceSql(replaceMap, sql);
         log.debug("execute sql : {}", executeSql);
-        return JpaUtil.execute(executeSql);
+        try {
+            return jdbcExecutor.findModeResult(executeSql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -37,7 +45,6 @@ public abstract class AbstractSqlBoostHandler extends AbstractBoostHandler {
     /**
      * 执行并组装sql语句
      * @param sql 要执行的sql语句
-     * @param fieldsName 组装名
      * @return
      */
     protected List<Map<String, Object>> executeSqlAuto(Map<String, Object> replaceMap, String sql) {
