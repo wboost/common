836b1372b4cc8ecf5972d728ed2581138e368b82
sunjw <jwsun@sunjwdeMacBook-Pro.local>
2019-01-17 10:33:07 +0800

    移除特定kylin.properties依赖

--- common-kylin/src/main/java/top/wboost/common/kylin/GlobalForKylinUtil.java
+++ common-kylin/src/main/java/top/wboost/common/kylin/GlobalForKylinUtil.java
@@ -16,14 +16,10 @@ public class GlobalForKylinUtil {
     public static final String KYLIN_SERVER_URL = "kylin.server.url";
     public static final String KYLIN_SERVER_NAME = "kylin.auth.name";
     public static final String KYLIN_SERVER_PASSWORD = "kylin.auth.password";
-    public static final String DEFAULT_PROPERTIES_PATH = "properties/kylin.properties";
 
-    public static final String kylinUrl = PropertiesUtil.getPropertyOrDefault(KYLIN_SERVER_URL,
-            PropertiesUtil.getProperty(KYLIN_SERVER_URL, DEFAULT_PROPERTIES_PATH));
-    public static final String kylinUserName = PropertiesUtil.getPropertyOrDefault(KYLIN_SERVER_NAME,
-            PropertiesUtil.getProperty(KYLIN_SERVER_NAME, DEFAULT_PROPERTIES_PATH));
-    public static final String kylinPassword = PropertiesUtil.getPropertyOrDefault(KYLIN_SERVER_PASSWORD,
-            PropertiesUtil.getProperty(KYLIN_SERVER_PASSWORD, DEFAULT_PROPERTIES_PATH));
+    public static final String kylinUrl = PropertiesUtil.getProperty(KYLIN_SERVER_URL);
+    public static final String kylinUserName = PropertiesUtil.getProperty(KYLIN_SERVER_NAME);
+    public static final String kylinPassword = PropertiesUtil.getProperty(KYLIN_SERVER_PASSWORD);
 
     /*public static final String kylinUrl = "http://192.168.51.70:7070/kylin";
     public static final String kylinUserName = "ADMIN";
